7c9cc9a85a1e1884e4adeba0608535137bead5df
Shock_N745 <Shockn745@gmail.com>
2015-07-20 21:14:29 +0200

    Implement first flip prototype

--- app/src/main/java/com/shockn745/wireinterview/MainActivity.java
+++ app/src/main/java/com/shockn745/wireinterview/MainActivity.java
@@ -3,6 +3,7 @@ package com.shockn745.wireinterview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     ImageView mMinion1;
+    ImageView mMinion2;
     SeekBar mSeekBar;
 
     @Override
@@ -20,25 +22,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        // Find views by id
         mMinion1 = (ImageView) findViewById(R.id.minion_1_image_view);
+        mMinion2 = (ImageView) findViewById(R.id.minion_2_image_view);
         mSeekBar = (SeekBar) findViewById(R.id.rotation_seek_bar);
+
+
+        // Set the camera distance to give a more natural flip movement
+        // And to respect the demo video
+        float scale = this.getResources().getDisplayMetrics().density;
+        float distance = 4000;
+        mMinion1.setCameraDistance(distance * scale);
+        mMinion2.setCameraDistance(distance * scale);
 
         mSeekBar.setMax(180);
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mMinion1.setRotationY(progress);
+                if (progress <= 90) {
+                    mMinion1.setRotationY(progress);
+                    mMinion1.setVisibility(View.VISIBLE);
+                    mMinion2.setVisibility(View.GONE);
+                } else if (progress > 90) {
+                    mMinion2.setRotationY(180 + progress);
+                    mMinion1.setVisibility(View.GONE);
+                    mMinion2.setVisibility(View.VISIBLE);
+                }
             }
+
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
+            public void onStartTrackingTouch(SeekBar seekBar) {}
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
     }
