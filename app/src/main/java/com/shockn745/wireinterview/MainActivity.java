dacc6372f099c0db45b405227f7e1ab3291d696c
Shock_N745 <Shockn745@gmail.com>
2015-07-20 19:57:38 +0200

    First test of rotation & seekbar

--- app/src/main/java/com/shockn745/wireinterview/MainActivity.java
+++ app/src/main/java/com/shockn745/wireinterview/MainActivity.java
@@ -2,14 +2,45 @@ package com.shockn745.wireinterview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    ImageView mMinion1;
+    SeekBar mSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mMinion1 = (ImageView) findViewById(R.id.minion_1_image_view);
+        mSeekBar = (SeekBar) findViewById(R.id.rotation_seek_bar);
+
+        mSeekBar.setMax(180);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mMinion1.setRotationY(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
 }
