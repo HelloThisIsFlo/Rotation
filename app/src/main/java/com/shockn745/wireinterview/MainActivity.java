ef4921f84f89a6b928b8995b8d957811d48b8a36
Shock_N745 <Shockn745@gmail.com>
2015-07-21 15:36:33 +0200

    Add Toolbar

--- app/src/main/java/com/shockn745/wireinterview/MainActivity.java
+++ app/src/main/java/com/shockn745/wireinterview/MainActivity.java
@@ -1,10 +1,14 @@
 package com.shockn745.wireinterview;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -23,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private static final int ANIM_DURATION = 6000;
     private static final int ROTATION_SPAN = 270;
 
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+
     private AnimatedMinion mMinion1;
     private AnimatedMinion mMinion2;
     private SeekBar mRotationSeekBar;
@@ -33,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add toolbar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         // Find views by id
         ImageView mMinion1ImageView = (ImageView) findViewById(R.id.minion_1_image_view);
